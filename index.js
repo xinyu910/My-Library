eac2ef6b3196444a0ea511bb89fbb96bd7b2b7f4
Xinyu Liu <xinyu910@students.cs.ubc.ca>
2023-05-31 23:17:35 -0400

    refactor to class structure

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 function Book(title, author, pages, isRead, img = "images/cover2.jpg") {
   this.title = title;
   this.author = author;
@@ -9,61 +10,122 @@ function Book(title, author, pages, isRead, img = "images/cover2.jpg") {
     return `${this.title} by ${this.author}, ${this.pages}, ${readString}`;
   };
 }
+*/
 
-let myBooks = [];
-
-function addBookToLibrary(title, author, pages, isRead, img) {
-  if (myBooks.some((book) => book.title === title && book.author === author)) {
-    return false;
+class Book {
+  constructor(
+    title = "Unknown",
+    author = "Unknown",
+    pages = 0,
+    isRead = false,
+    img = "images/cover2.jpg"
+  ) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    this.img = img;
   }
-  let book;
-  if (img === "null") {
-    book = new Book(title, author, pages, isRead);
-  } else {
-    book = new Book(title, author, pages, isRead, img);
-  }
-  myBooks.push(book);
-  return true;
 }
 
-function removeFromLibrary(title, author) {
-  myBooks = myBooks.filter(
-    (book) => book.title !== title && book.author !== author
-  );
-}
+class Library {
+  constructor() {
+    this.myBooks = [];
+  }
 
-function getBook(title, author) {
-  for (let book of myBooks) {
-    if (book.title === title && book.author === author) {
-      return book;
+  addBookToLibrary(book) {
+    if (this.bookExist(book)) {
+      return false;
     }
+    this.myBooks.push(book);
+    return true;
   }
-  return null;
+
+  bookExist(newBook) {
+    return this.myBooks.some(
+      (book) => book.title === newBook.title && book.author === newBook.author
+    );
+  }
+
+  removeFromLibrary(thisBook) {
+    myBooks = myBooks.filter(
+      (book) => book.title !== thisBook.title && book.author !== thisBook.author
+    );
+  }
 }
 
-function toggleFn(e) {
-  const id = e.target.dataset.id;
-  if (myBooks[id].isRead) {
-    myBooks[id].isRead = false;
-    e.target.previousSibling.innerText = "Status: In Progress";
-    e.target.previousSibling.style.color = "#cf3a08";
-  } else {
-    myBooks[id].isRead = true;
-    e.target.previousSibling.innerText = "Status: Read";
-    e.target.previousSibling.style.color = "#4f5949";
-  }
-}
-
-function removeBook(e) {
-  const id = e.target.dataset.id;
-  myBooks.splice(id, 1);
-  reloadBooks();
-}
+const library = new Library();
 
 const addGridTemplate = document.getElementById("add-grid-template");
 const booksGrid = document.querySelector(".books-grid");
+const sampleBtn = document.getElementById("sample");
+//add-form
+const addBtn = document.querySelector(".add-button");
+const overlay = document.querySelector(".overlay");
+const modal = document.querySelector(".add-book-modal");
+const closeForm = document.querySelector(".close-form");
+const submitForm = document.querySelector(".add-book-form");
+const error = document.querySelector(".error-message");
 
-function createGridItem(book, i) {
+//let myBooks = [];
+
+//Form listners and helper functiosn
+const popForm = () => {
+  submitForm.reset();
+  error.classList.remove("active");
+  modal.classList.add("active");
+  overlay.classList.add("active");
+};
+addBtn.addEventListener("click", popForm, false);
+
+closeForm.addEventListener("click", () => {
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+});
+
+const getBase64 = (file) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+};
+const getNewBook = async () => {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const isRead = document.getElementById("isRead").checked;
+  const image = document.getElementById("cover");
+  if (image.files && image.files[0]) {
+    const img = await getBase64(image.files[0]);
+    return new Book(title, author, pages, isRead, img);
+  } else {
+    return new Book(title, author, pages, isRead);
+  }
+};
+
+//rerender books helper
+const toggleFn = (e) => {
+  const id = e.target.dataset.id;
+  if (library.myBooks[id].isRead) {
+    library.myBooks[id].isRead = false;
+    e.target.previousSibling.innerText = "Status: In Progress";
+    e.target.previousSibling.style.color = "#cf3a08";
+  } else {
+    library.myBooks[id].isRead = true;
+    e.target.previousSibling.innerText = "Status: Read";
+    e.target.previousSibling.style.color = "#4f5949";
+  }
+};
+
+const removeBook = (e) => {
+  const id = e.target.dataset.id;
+  library.myBooks.splice(id, 1);
+  reloadBooks();
+};
+
+const createGridItem = (book, i) => {
   const gridItem = document.createElement("div");
   const bookInfo = document.createElement("div");
   const image = document.createElement("img");
@@ -114,114 +176,78 @@ function createGridItem(book, i) {
   bookInfo.appendChild(deleteBtn);
   gridItem.appendChild(bookInfo);
   booksGrid.appendChild(gridItem);
-}
+};
 
-function getBase64(file) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = (error) => reject(error);
-  });
-}
-
-function popForm() {
-  submitForm.reset();
-  error.classList.remove("active");
-  modal.classList.add("active");
-  overlay.classList.add("active");
-}
-
-function reloadBooks() {
+const reloadBooks = () => {
   booksGrid.innerHTML = "";
   booksGrid.innerHTML += addGridTemplate.innerHTML;
-  const addBtn = document.querySelector(".add-button");
-  addBtn.addEventListener("click", popForm, false);
-  for (let i = 0; i < myBooks.length; i++) {
-    createGridItem(myBooks[i], i);
+  const newAddBtn = document.querySelector(".add-button");
+  newAddBtn.addEventListener("click", popForm, false);
+  for (let i = 0; i < library.myBooks.length; i++) {
+    createGridItem(library.myBooks[i], i);
   }
-}
+};
 
-const addBtn = document.querySelector(".add-button");
-const overlay = document.querySelector(".overlay");
-const modal = document.querySelector(".add-book-modal");
-const closeForm = document.querySelector(".close-form");
-const submitForm = document.querySelector(".add-book-form");
-const error = document.querySelector(".error-message");
-const sampleBtn = document.getElementById("sample");
-
-addBtn.addEventListener("click", popForm, false);
-
-closeForm.addEventListener("click", function () {
-  modal.classList.remove("active");
-  overlay.classList.remove("active");
-});
-
-submitForm.addEventListener("submit", function (e) {
+submitForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  const pages = document.getElementById("pages").value;
-  const isRead = document.getElementById("isRead").checked;
-  const image = document.getElementById("cover");
-  let img;
-  if (image.files && image.files[0]) {
-    getBase64(image.files[0]).then((data) => {
-      img = data;
-      if (addBookToLibrary(title, author, pages, isRead, img) === false) {
-        error.classList.add("active");
-      } else {
-        modal.classList.remove("active");
-        overlay.classList.remove("active");
-        reloadBooks();
-      }
-    });
-  } else {
-    if (addBookToLibrary(title, author, pages, isRead) === false) {
+  let newBook;
+  getNewBook().then((book) => {
+    newBook = book;
+    if (library.addBookToLibrary(newBook) === false) {
       error.classList.add("active");
     } else {
       modal.classList.remove("active");
       overlay.classList.remove("active");
       reloadBooks();
     }
-  }
+  });
 });
 
-sampleBtn.addEventListener("click", function () {
-  addBookToLibrary(
-    "The Hobbit",
-    "J.R.R. Tolkien",
-    295,
-    false,
-    "images/The-Hobbit.jpg"
+sampleBtn.addEventListener("click", () => {
+  library.addBookToLibrary(
+    new Book(
+      "The Hobbit",
+      "J.R.R. Tolkien",
+      295,
+      false,
+      "images/The-Hobbit.jpg"
+    )
   );
-  addBookToLibrary(
-    "The Last Thing He Told Me",
-    "Laura Dave",
-    320,
-    false,
-    "images/Last-Thing.jpg"
+  library.addBookToLibrary(
+    new Book(
+      "The Last Thing He Told Me",
+      "Laura Dave",
+      320,
+      false,
+      "images/Last-Thing.jpg"
+    )
   );
-  addBookToLibrary(
-    "Left to Fear",
-    "Blake Pierce",
-    223,
-    false,
-    "images/Left-to-Fear.jpg"
+  library.addBookToLibrary(
+    new Book(
+      "Left to Fear",
+      "Blake Pierce",
+      223,
+      false,
+      "images/Left-to-Fear.jpg"
+    )
   );
-  addBookToLibrary(
-    "Malibu Rising",
-    "Taylor Jenkins Reid",
-    384,
-    false,
-    "images/Malibu-Rising.jpg"
+  library.addBookToLibrary(
+    new Book(
+      "Malibu Rising",
+      "Taylor Jenkins Reid",
+      384,
+      false,
+      "images/Malibu-Rising.jpg"
+    )
   );
-  addBookToLibrary(
-    "All the Light We Cannot See",
-    "Anthony Doerrd",
-    544,
-    false,
-    "images/All-the-Night.jpg"
+  library.addBookToLibrary(
+    new Book(
+      "All the Light We Cannot See",
+      "Anthony Doerrd",
+      544,
+      false,
+      "images/All-the-Night.jpg"
+    )
   );
   reloadBooks();
 });
